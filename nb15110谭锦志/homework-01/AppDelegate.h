edfb145e97cb8e89a307d261e51d8f710f557a34
apanoo <apanoo@126.com>
2015-10-07 15:00:17 +0800

    finish homework 01

---
+++ nb15110谭锦志/homework-01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  homework-01
+//
+//  Created by apanoo on 15/10/7.
+//  Copyright © 2015年 apanoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
