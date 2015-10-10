a8cb01e33a473e875993bfdba6051fe12534c4c9
861008761 <YCFile@9D352A96-ADDD-484B-848A-636DBC12187A._icloud-ds._tcp.local>
2015-10-10 23:46:27 +0800

    first homework

---
+++ 21551186杨超/Demo1/Demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo1
+//
+//  Created by YC－MacBookPro on 15/10/5.
+//  Copyright (c) 2015年 YC－MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21551186杨超/Demo1/Demo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo1
+//
+//  Created by YC－MacBookPro on 15/10/5.
+//  Copyright (c) 2015年 YC－MacBookPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
