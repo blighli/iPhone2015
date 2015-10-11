924fbf48b17cf4dcec1cb1bab4fb4034910d2142
LYHYOKI <lyhyoki@yeah.net>
2015-10-12 01:25:33 +0800

    [bugfix] error dir removed

---
+++ nb15074刘溢辉/demo0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo0
+//
+//  Created by LYH on 15/10/11.
+//  Copyright © 2015年 LYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
