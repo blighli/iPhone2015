e750adde44890ca9dd6828a2ca8af3180ee1ef76
wp <wp@oliverAM.local>
2015-10-11 21:10:33 +0800

    吴桐的第一次作业

---
+++ nb15038吴桐/MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by wp on 15/10/4.
+//  Copyright © 2015年 wt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
