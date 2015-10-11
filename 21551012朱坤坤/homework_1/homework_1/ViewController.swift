f8d65ac03c589341d456f19ca6cb01e904046670
zhukunkun <k525245669@sina.com>
2015-10-12 00:44:21 +0800

    first homework

--- 21551012朱坤坤/homework_1/homework_1/ViewController.swift
+++ 21551012朱坤坤/homework_1/homework_1/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var mlabel: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnClick(sender: UIButton){
+        let btnText = sender.currentTitle!
+        mlabel.text = "You clicked \(btnText)"
+        
     }
 
 
