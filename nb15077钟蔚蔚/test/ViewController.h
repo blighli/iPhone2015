529a75772c3c9978f48280b105c1a2f65a81b7be
xuke <m_xuke@163.com>
2015-10-14 13:05:37 +0800

    zhongweiwei

---
+++ nb15077钟蔚蔚/test/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by doubi on 15/10/14.
+//  Copyright (c) 2015年 cstlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+- (IBAction)buttonPress:(UIButton *)sender;
+
+
+@end
+
