31709d23e7752d6ba69f1e72fca483920627eac2
Veronica159 <2116778705@qq.com>
2015-10-15 00:44:20 +0800

    龚天天work1

---
+++ NB15012龚天天/work1/FirstHomeword/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstHomeword
+//
+//  Created by 天天 龚 on 15/10/14.
+//  Copyright © 2015年 天天 龚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
