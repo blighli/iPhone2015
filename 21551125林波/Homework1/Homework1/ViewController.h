a3ea502ddaaf72282b3df50494c66735fc6eb5d2
linbo1993 <linboahu@163.com>
2015-10-18 09:53:13 +0800

    林波的第一次作业

---
+++ 21551125林波/Homework1/Homework1/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Homework1
+//
+//  Created by linbo on 15/10/17.
+//  Copyright © 2015年 linbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *statusTextView;
+- (IBAction)leftButtonPressed:(UIButton *)sender;
+
+
+@end
+
