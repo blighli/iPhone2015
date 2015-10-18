fa3cee9a37b76ebadae5f9380b2b2bef9d18fcbe
zhangshang <1562718553@qq.com>
2015-10-18 15:19:07 +0800

    zhangkai_homework

---
+++ 21551016张凯/HelloIOS/HelloIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloIOS
+//
+//  Created by 5hen on 15/10/13.
+//  Copyright (c) 2015年 5hen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
