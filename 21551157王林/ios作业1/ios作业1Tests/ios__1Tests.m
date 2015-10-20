c0d7150818b7a4f97130d1d1e88c0eb653cc3ba3
陈受恩 <2580358360@qq.com>
2015-10-20 23:19:16 +0800

    ios作业

---
+++ 21551157王林/ios作业1/ios作业1Tests/ios__1Tests.m
@@ -0,0 +1,39 @@
+//
+//  ios__1Tests.m
+//  ios作业1Tests
+//
+//  Created by sjaiwl on 15/10/6.
+//  Copyright © 2015年 sjaiwl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios__1Tests : XCTestCase
+
+@end
+
+@implementation ios__1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
