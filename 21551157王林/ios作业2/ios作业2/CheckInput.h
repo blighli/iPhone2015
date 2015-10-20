a210b7ec5d4944a1b4efe718545483f02eaf6b9b
王林 <761034053@qq.com>
2015-10-20 20:51:43 +0800

    ios作业2

---
+++ 21551157王林/ios作业2/ios作业2/CheckInput.h
@@ -0,0 +1,16 @@
+//
+//  CheckInput.h
+//  ios作业2
+//
+//  Created by 王林 on 15/10/20.
+//  Copyright © 2015年 sjaiwl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckInput : NSObject
+
+//判断某个字符串都是纯数字
++ (BOOL) checkStringIsNumber:(NSString *) string;
+
+@end
