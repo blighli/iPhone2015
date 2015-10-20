daf8f703e1fad2c27a6496482049db8b6efa56a6
zengl267 <304891441@qq.com>
2015-10-20 15:10:39 +0800

    homework1 曾琳

---
+++ nb15126曾琳/homework1/homework1/homework1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  homework1
+//
+//  Created by Mac on 15/10/20.
+//  Copyright © 2015年 zenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
