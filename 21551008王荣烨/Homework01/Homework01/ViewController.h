1ce2b561dc79b0ce259566af419a31e5a44a4b3d
Tiphardor <2075151635@qq.com>
2015-10-25 15:27:22 +0800

    第一次和第二次作业

---
+++ 21551008王荣烨/Homework01/Homework01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework01
+//
+//  Created by Hardor-PRO on 15-10-21.
+//  Copyright (c) 2015年 Hardor-PRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)button1pressed:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *statuslabel;
+
+@end
