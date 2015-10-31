093a4d07125cc8a015eba2a9c9368542a870c050
alezai <alezaizai@163.com>
2015-10-31 10:25:57 +0800

    林永乐-Homework03

---
+++ 21551175林永乐/iPhone-Homework03/iPhone-Homework03/IP3ListInfo.h
@@ -0,0 +1,15 @@
+//
+//  IP3ListInfo.h
+//  iPhone-Homework03
+//
+//  Created by alezai on 15/10/30.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IP3ListInfo : NSObject<NSCoding>
+
+@property (strong, nonatomic) NSArray *listArray;
+
+@end
