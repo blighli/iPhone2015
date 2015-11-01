29c92846d435c36f0b3fb34713b92749ecf62127
chenyufeng <503434355@qq.com>
2015-11-02 01:39:25 +0800

    第二次

---
+++ 21551202陈宇峰/iOS第二次作业/源码/MyCalendar/MyCalendar/JudgeNumberRegex.h
@@ -0,0 +1,15 @@
+//
+//  JudgeNumberRegex.h
+//  MyCalendarDemo
+//
+//  Created by chenyufeng on 15/10/19.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JudgeNumberRegex : NSObject
+
+- (BOOL) isNumber:(NSString*)str;
+
+@end
---
+++ 21551202陈宇峰/iOS第二次作业/源码/MyCalendar/MyCalendar/PrintYearAndMonth.h
@@ -0,0 +1,18 @@
+//
+//  PrintYearAndMonth.h
+//  MyCalendarDemo
+//
+//  Created by chenyufeng on 15/10/19.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrintYearAndMonth : NSObject
+
+- (void) printmonth:(int)year yue:(int)yue;
+- (void) printyear:(int)year;
+
+
+
+@end
