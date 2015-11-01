1012aa6624d7a043c2d50f341bdd2bd28a8a3e2a
zhuzhu <zhuzhu@abcdeMac.local>
2015-11-01 21:26:27 +0800

    asdasd

---
+++ 21551055韩梦薇/HMWHomework3/HMWHomework3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HMWHomework3
+//
+//  Created by zhuzhu on 15/10/31.
+//  Copyright © 2015年 wequeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
