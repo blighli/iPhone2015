68059fde5a5600a87c7e7ed8f460b535e86a508b
zimhy <466916676@qq.com>
2015-11-01 14:50:27 +0800

    homework 3

---
+++ 20151046张梦海/homework3/helloworld_zimhy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld_zimhy
+//
+//  Created by menghaizhang on 15/11/1.
+//  Copyright © 2015年 menghaizhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
