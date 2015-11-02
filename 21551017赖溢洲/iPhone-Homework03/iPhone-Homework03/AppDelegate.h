086ae817bfd10e90591008be6c8a0572791ce66c
LaiYizhou <lai_yizhou@163.com>
2015-11-02 20:54:27 +0800

    homework03

---
+++ 21551017赖溢洲/iPhone-Homework03/iPhone-Homework03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhone-Homework03
+//
+//  Created by 赖溢洲 on 15/10/30.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21551017赖溢洲/iPhone-Homework03/iPhone-Homework03/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhone-Homework03
+//
+//  Created by 赖溢洲 on 15/10/30.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
