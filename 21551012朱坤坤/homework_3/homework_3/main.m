ad4a9f42b7007803d08612a1720d6c350c6ea056
zhukun <k525245669@sina.com>
2015-11-02 14:36:14 +0800

    add homework_3

---
+++ 21551012朱坤坤/homework_3/homework_3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  homework_3
+//
+//  Created by zhukun on 15/11/2.
+//  Copyright (c) 2015年 zhukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
