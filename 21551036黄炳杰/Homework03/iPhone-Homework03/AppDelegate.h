828898fbb39f347c84e215624c2858fbd97ed066
黄炳杰 <huangbingjie@Bings-MacBook-Pro.local>
2015-11-04 00:28:39 +0800

    01-03Homework

---
+++ 21551036黄炳杰/Homework03/iPhone-Homework03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhone-Homework03
+//
+//  Created by 黄炳杰 on 15/10/30.
+//  Copyright © 2015年 黄炳杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
