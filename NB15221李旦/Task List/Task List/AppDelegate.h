381b247958d5dddb3f77e7bde81b20c0074ea3c7
Muzry <kkcld@sina.com>
2015-11-03 10:28:28 +0800

    第三次作业

---
+++ NB15221李旦/Task List/Task List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task List
+//
+//  Created by LiDan on 15/11/3.
+//  Copyright © 2015年 com.lidan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
