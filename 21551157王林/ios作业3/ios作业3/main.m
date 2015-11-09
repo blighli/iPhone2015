e5358c46cb982411e3e41ab0661a711882901eef
wwlzz <wlz@appest.com>
2015-11-09 19:30:15 +0800

    王林iOS作业

---
+++ 21551157王林/ios作业3/ios作业3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios作业3
+//
+//  Created by Appest on 15/11/1.
+//  Copyright © 2015年 Appest Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ 21551157王林/ios作业3/ios作业3Tests/ios__3Tests.m
@@ -0,0 +1,39 @@
+//
+//  ios__3Tests.m
+//  ios作业3Tests
+//
+//  Created by Appest on 15/11/1.
+//  Copyright © 2015年 Appest Limited. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios__3Tests : XCTestCase
+
+@end
+
+@implementation ios__3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
