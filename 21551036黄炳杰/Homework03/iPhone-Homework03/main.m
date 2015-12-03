1498c24cabaf6d7374314bca5212da61d96d3106
Jack King <q347199174@gmail.com>
2015-12-03 16:35:59 +0800

    黄炳杰三次作业重新提交

---
+++ 21551036黄炳杰/Homework03/iPhone-Homework03/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhone-Homework03
+//
+//  Created by alezai on 15/10/30.
+//  Copyright © 2015年 alezai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
