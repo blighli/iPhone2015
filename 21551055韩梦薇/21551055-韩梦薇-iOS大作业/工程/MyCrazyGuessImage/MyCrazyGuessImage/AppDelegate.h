3a115ec8e766016ed5914e83b10ea5ef748eee3f
zhuzhu <576581941@qq.com>
2015-12-30 23:13:09 +0800

    ios大作业

---
+++ 21551055韩梦薇/21551055-韩梦薇-iOS大作业/工程/MyCrazyGuessImage/MyCrazyGuessImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCrazyGuessImage
+//
+//  Created by zhuzhu on 15/11/25.
+//  Copyright © 2015年 wequeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
