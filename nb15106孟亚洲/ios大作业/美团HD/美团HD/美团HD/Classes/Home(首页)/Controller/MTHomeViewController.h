af8fb4be74a2f857de0a78c53926760ef5ef0c8e
hubin <385166229@qq.com>
2015-12-30 22:47:43 +0800

    ios大作业

---
+++ nb15106孟亚洲/ios大作业/美团HD/美团HD/美团HD/Classes/Home(首页)/Controller/MTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTHomeViewController.h
+//  美团HD
+//
+//  Created by cstlab on 15/11/8.
+//  Copyright © 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTHomeViewController : UICollectionViewController
+
+@end
---
+++ nb15106孟亚洲/ios大作业/美团HD/美团HD/美团HD/Classes/Main(主要)/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美团HD
+//
+//  Created by cstlab on 15/11/5.
+//  Copyright © 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
