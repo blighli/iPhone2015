cb96359f32ffcf427c5f5d310fea1346e0816ad4
linyuncong <15257428751@sina.com>
2015-12-31 23:38:40 +0800

    大作业
    大作业

---
+++ nb150232林运从/大作业/calculator/calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by BreazeMago on 14-6-23.
+//  Copyright (c) 2014年 BreazeMago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nb150232林运从/大作业/calculator/calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  calculator
+//
+//  Created by BreazeMago on 14-6-23.
+//  Copyright (c) 2014年 BreazeMago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
