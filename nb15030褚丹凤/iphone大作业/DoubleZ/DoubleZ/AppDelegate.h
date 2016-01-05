8908c60c6aed5a6a7f634a85ef1c91fe8fbea836
cdf216 <cdf216@qq.com>
2016-01-05 09:50:55 +0000

    作业提交

---
+++ nb15030褚丹凤/iphone大作业/DoubleZ/DoubleZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubleZ
+//
+//  Created by cdf on 15/12/29.
+//  Copyright © 2015年 cdf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
