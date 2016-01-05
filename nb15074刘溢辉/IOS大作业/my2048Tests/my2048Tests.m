ab9fe0a3d4e63544ba39a18609ff234406d2e0f6
lyhyoki@yeah.net <cdf216@qq.com>
2016-01-05 10:09:11 +0000

    作业提交

---
+++ nb15074刘溢辉/IOS大作业/my2048Tests/my2048Tests.m
@@ -0,0 +1,34 @@
+//
+//  my2048Tests.m
+//  my2048Tests
+//
+//  Created by LYH on 15/12/29.
+//  Copyright © 2015年 LYH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface my2048Tests : XCTestCase
+
+@end
+
+@implementation my2048Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
